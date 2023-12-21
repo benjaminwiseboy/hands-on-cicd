428bee8761542307757b2517ccc1590b5d64b8a6
Clovis MENEGAIN <clovis.menegain@revolve.team>
2023-12-21 10:25:39 +0100

    Update file backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "testhandsoncicd"
-    key    = "handons/gitlab/api-gateway/terraform.tfstates"
+    bucket = "backend-test-terraform"
+    key    = "handons/api-gateway/terraform.tfstates"
     region = "eu-west-1"
   }
 }
