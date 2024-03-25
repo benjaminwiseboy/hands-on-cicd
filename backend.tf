402d2b437b8c1b3420fa87e4d5354bc4eae7e775
clovismenegain <clovis.menegain@revolve.team>
2024-03-25 15:46:28 +0100

    fix: bucket backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend-gitlab-terraform"
+    bucket = "backend-test-terraform"
     key    = "handons/gitlab/terraform.tfstates"
     region = "eu-west-1"
   }
